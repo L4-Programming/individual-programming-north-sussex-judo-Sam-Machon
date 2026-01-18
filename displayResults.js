e5b56e46b1021010d49697053fa5dee996ce65cc
Sam-Machon <sam.machon@students.plymouth.ac.uk>
2026-01-18 22:01:46 +0000

    added mobile screen size styling

--- displayResults.js
+++ displayResults.js
@@ -1,9 +1,30 @@
 export function displayResults(result, output, userCategory) {
-  let results = document.querySelector("#output");
+  let resultsDesktop = document.querySelector("#output-desktop");
   console.log(output);
   console.log(userCategory);
 
-  results.innerHTML = `
+  resultsDesktop.innerHTML = `
+    <dl>
+      <dt>Athlete Name</dt>
+      <dd>${result.userName}</dd>
+
+      <dt>Costs:</dt>
+      <dd>Competition Entry Cost £${output.competitionCost}</dd>
+      <dd> Private Coaching Cost £${output.coachingCost}</dd>
+
+      <dt>Total Cost for the Month</dt>
+      <dd>£${output.totalCost}</dd>
+
+      <dt>Weight Category</dt>
+      <dd>${userCategory}</dd>
+
+  `;
+
+  let resultsMobile = document.querySelector("#output-mobile");
+  console.log(output);
+  console.log(userCategory);
+
+  resultsMobile.innerHTML = `
     <dl>
       <dt>Athlete Name</dt>
       <dd>${result.userName}</dd>
